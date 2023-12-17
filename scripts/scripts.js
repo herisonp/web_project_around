5c0998d7bfaa711c52616ac340f700b8a7f6beb3
Herison Pereira <herisonup@gmail.com>
2023-12-17 15:46:12 -0300

    fix: add code formatting and .editorconfig

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,17 +9,17 @@ let nameText = document.querySelector(".profile__name");
 let jobText = document.querySelector(".profile__about");
 
 function togglePopup() {
-	let popup = document.querySelector(".popup");
-	popup.classList.toggle("popup_opened");
-	nameInput.value = nameText.textContent;
-	jobInput.value = jobText.textContent;
+  let popup = document.querySelector(".popup");
+  popup.classList.toggle("popup_opened");
+  nameInput.value = nameText.textContent;
+  jobInput.value = jobText.textContent;
 }
 
 function handleProfileFormSubmit(evt) {
-	evt.preventDefault();
-	nameText.textContent = nameInput.value;
-	jobText.textContent = jobInput.value;
-	togglePopup();
+  evt.preventDefault();
+  nameText.textContent = nameInput.value;
+  jobText.textContent = jobInput.value;
+  togglePopup();
 }
 
 formElement.addEventListener("submit", handleProfileFormSubmit);
